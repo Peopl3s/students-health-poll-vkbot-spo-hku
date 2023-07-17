c4d9551712226730e4152f4ebb17713a7c8d58f4
Peopl3s <nikposd@mail.ru>
2023-07-17 13:07:01 +0300

    Update config.py
    Add env variables

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 settings = {
-    'TOKEN':'03333e704aecb132aaaab692ee6e06ce4bce159d0839f49afd3e935e247dd3cefee3450d9e89301b23f5b',
-    'VK_GROUP_ID': 204175375,
-    'LOG_FILE': 'poll-bot.log'
+    'TOKEN': os.getenv('TOKEN'),
+    'VK_GROUP_ID': os.getenv('VK_GROUP_ID'),
+    'LOG_FILE': os.getenv('LOG_FILE')
 }
