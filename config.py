c5dab95069b0927f00568f4208e9ca5715936da5
Peopl3s <nikposd@mail.ru>
2023-07-17 13:36:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ load_dotenv()
 settings = {
     'TOKEN': os.getenv('TOKEN'),
     'VK_GROUP_ID': os.getenv('VK_GROUP_ID'),
-    'LOG_FILE': os.getenv('LOG_FILE')
+    'LOG_FILE': os.getenv('LOG_FILE'),
+    'CREDS_FILE': os.getenv('CREDS_FILE')
 }
